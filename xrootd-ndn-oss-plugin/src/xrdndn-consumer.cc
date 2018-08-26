b6871cf550c918b4557698efd20607c4b28163a6
Catalin Iordache <catalinn.iordache@gmail.com>
2018-08-26 23:40:40 +0300

    XrdNdnOss: Added onTimeout, onNack and validation handler in Consumer#2

--- xrootd-ndn-oss-plugin/src/xrdndn-consumer.cc
+++ xrootd-ndn-oss-plugin/src/xrdndn-consumer.cc
@@ -18,119 +18,183 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>. *
  **************************************************************************/
 
+#include <functional>
 #include <iostream>
 
-#include "xrdndn-common.hh"
 #include "xrdndn-consumer.hh"
 #include "xrdndn-utils.hh"
 
 using namespace ndn;
 
 namespace xrdndn {
-Consumer::Consumer() {}
-
-Consumer::~Consumer() { m_face.shutdown(); }
-
-void Consumer::onNack(const Interest &interest, const lp::Nack &nack) {
-    std::cout << "xrdndnconsumer: Received NACK with reason: "
-              << nack.getReason() << " for interest " << interest << std::endl;
-}
-
-void Consumer::onTimeout(const Interest &interest) {
-    std::cout << "xrdndnconsumer: Timeout for interest: " << interest
-              << std::endl;
-}
-
-int Consumer::Open(std::string path) {
-    Interest openInterest(Utils::getInterestUri(SystemCalls::open, path));
-    openInterest.setInterestLifetime(100_s); // Change this on release
-    openInterest.setMustBeFresh(true);
-
-    int ret = -1;
-    auto openOnData = [&](const Interest &interest, const Data &data) {
-        ret = this->getIntegerFromData(data);
-        std::cout << "xrdndnconsumer: Open file: " << path
-                  << " with error code: " << ret << std::endl;
-    };
-
-    m_face.expressInterest(openInterest, bind(openOnData, _1, _2),
-                           bind(&Consumer::onNack, this, _1, _2),
-                           bind(&Consumer::onTimeout, this, _1));
-
-    std::cout << "xrdndnconsumer: Sending open file interest: " << openInterest
-              << std::endl;
-    m_face.processEvents();
-
-    return ret;
-}
-
-int Consumer::Close(std::string path) {
-    Interest openInterest(Utils::getInterestUri(SystemCalls::close, path));
-    openInterest.setInterestLifetime(100_s); // Change this on release
-    openInterest.setMustBeFresh(true);
-
-    int ret = -1;
-    auto closeOnData = [&](const Interest &interest, const Data &data) {
-        ret = this->getIntegerFromData(data);
-        std::cout << "xrdndnconsumer: Close file: " << path
-                  << " with error code: " << ret << std::endl;
-    };
-
-    m_face.expressInterest(openInterest, bind(closeOnData, _1, _2),
-                           bind(&Consumer::onNack, this, _1, _2),
-                           bind(&Consumer::onTimeout, this, _1));
-
-    std::cout << "xrdndnconsumer: Sending close file interest: " << openInterest
-              << std::endl;
-    m_face.processEvents();
-
-    return ret;
-}
-
-ssize_t Consumer::Read(void *buff, off_t offset, size_t blen,
-                       std::string path) {
-
-    auto fileReader = shared_ptr<FileReader>(new FileReader(m_face));
-    return fileReader->Read(buff, offset, blen, path);
-}
-
-int Consumer::getIntegerFromData(const Data &data) {
-    int value = readNonNegativeInteger(data.getContent());
-    return data.getContentType() == xrdndn::tlv::negativeInteger ? -value
-                                                                 : value;
-}
-
-FileReader::FileReader(Face &face) : m_face(face) {
+Consumer::Consumer()
+    : m_scheduler(m_face.getIoService()),
+      m_validator(security::v2::getAcceptAllValidator()), m_nTimeouts(0),
+      m_nNacks(0), m_retOpen(-1), m_retClose(-1) {
     m_bufferedData.clear();
     m_nextToCopy = 0;
     m_buffOffset = 0;
 }
 
-FileReader::~FileReader() { m_bufferedData.clear(); }
-
-void FileReader::onNack(const Interest &interest, const lp::Nack &nack) {
-    std::cout << "xrdndnconsumer: Received NACK with reason: "
-              << nack.getReason() << " for interest " << interest << std::endl;
-
-    // TODO
+Consumer::~Consumer() {
+    m_face.removeAllPendingInterests();
+    m_face.shutdown();
+    m_bufferedData.clear();
 }
 
-void FileReader::onTimeout(const Interest &interest) {
+const Interest Consumer::composeInterest(const Name name) {
+    Interest interest(name);
+    interest.setInterestLifetime(DEFAULT_INTEREST_LIFETIME);
+    interest.setMustBeFresh(true);
+    return interest;
+}
+
+void Consumer::expressInterest(const Interest &interest,
+                               const SystemCalls call) {
+    std::function<void(const Interest &interest, const Data &data)> onData;
+    switch (call) {
+    case (SystemCalls::open):
+        onData = bind(&Consumer::onOpenData, this, _1, _2);
+        break;
+    case (SystemCalls::close):
+        onData = bind(&Consumer::onCloseData, this, _1, _2);
+        break;
+    case (SystemCalls::read):
+        onData = bind(&Consumer::onReadData, this, _1, _2);
+        break;
+    default:
+        return;
+    }
+
+    m_face.expressInterest(interest, onData,
+                           bind(&Consumer::onNack, this, _1, _2, call),
+                           bind(&Consumer::onTimeout, this, _1, call));
+}
+
+void Consumer::onNack(const Interest &interest, const lp::Nack &nack,
+                      const SystemCalls call) {
+    if (m_nNacks > MAX_RETRIES) {
+        std::cout
+            << "xrdndnconsumer: Reached the maximum number of nack retries: "
+            << m_nNacks << " while retrieving data for: " << interest
+            << std::endl;
+        return;
+    }
+
+    ++m_nNacks;
+
+    Interest newInterest(interest);
+    newInterest.refreshNonce();
+
+    switch (nack.getReason()) {
+    case lp::NackReason::DUPLICATE:
+        this->expressInterest(newInterest, call);
+        break;
+    case lp::NackReason::CONGESTION:
+        m_scheduler.scheduleEvent(
+            CONGESTION_TIMEOUT,
+            bind(&Consumer::expressInterest, this, interest, call));
+        break;
+    default:
+        std::cout << "xrdndnconsumer: Received NACK with reason: "
+                  << nack.getReason() << " for interest " << interest
+                  << std::endl;
+        break;
+    }
+}
+
+void Consumer::onTimeout(const Interest &interest, const SystemCalls call) {
     std::cout << "xrdndnconsumer: Timeout for interest: " << interest
               << std::endl;
+    if (m_nTimeouts > MAX_RETRIES) {
+        std::cout
+            << "xrdndnconsumer: Reached the maximum number of timeout retries: "
+            << m_nTimeouts << " while retrieving data for: " << interest
+            << std::endl;
+        return;
+    }
 
-    // TODO
+    ++m_nTimeouts;
+
+    Interest newInterest(interest);
+    newInterest.refreshNonce();
+    this->expressInterest(newInterest, call);
 }
 
-ssize_t FileReader::Read(void *buff, off_t offset, size_t blen,
-                         std::string path) {
+void Consumer::onOpenData(const Interest &interest, const Data &data) {
+    m_validator.validate(
+        data,
+        [this, interest](const Data &data) {
+            m_retOpen = this->getIntegerFromData(data);
+            std::cout << "xrdndnconsumer: Open file: " << interest
+                      << " with error code: " << m_retOpen << std::endl;
+        },
+        [](const Data &, const security::v2::ValidationError &error) {
+            std::cout
+                << "xrdndnconsumer: Error while validating data for open: "
+                << error.getInfo() << std::endl;
+        });
+}
 
-    auto onData = [&](const Interest &interest, const Data &data) {
-        std::cout << "xrdndnconsumer: Read file: Received data for: "
-                  << interest << std::endl;
-        m_bufferedData[0] = data.shared_from_this();
-        this->saveDataInOrder(buff, offset, blen);
-    };
+int Consumer::Open(std::string path) {
+    Interest openInterest =
+        this->composeInterest(Utils::getInterestUri(SystemCalls::open, path));
+    this->expressInterest(openInterest, SystemCalls::open);
+
+    std::cout << "xrdndnconsumer: Sending open file interest: " << openInterest
+              << std::endl;
+    m_face.processEvents();
+
+    return m_retOpen;
+}
+
+void Consumer::onCloseData(const Interest &interest, const Data &data) {
+    m_validator.validate(
+        data,
+        [this, interest](const Data &data) {
+            m_retClose = this->getIntegerFromData(data);
+            std::cout << "xrdndnconsumer: Close file: " << interest
+                      << " with error code: " << m_retClose << std::endl;
+        },
+        [](const Data &, const security::v2::ValidationError &error) {
+            std::cout
+                << "xrdndnconsumer: Error while validating data for close: "
+                << error.getInfo() << std::endl;
+        });
+}
+
+int Consumer::Close(std::string path) {
+    Interest openInterest =
+        this->composeInterest(Utils::getInterestUri(SystemCalls::close, path));
+    this->expressInterest(openInterest, SystemCalls::close);
+
+    std::cout << "xrdndnconsumer: Sending close file interest: " << openInterest
+              << std::endl;
+    m_face.processEvents();
+
+    return m_retClose;
+}
+
+void Consumer::onReadData(const ndn::Interest &interest,
+                          const ndn::Data &data) {
+    std::cout << "xrdndnconsumer: Read file: Received data for: " << interest
+              << std::endl;
+
+    auto dataPtr = data.shared_from_this();
+    m_validator.validate(
+        data,
+        [this, dataPtr](const Data &data) {
+            m_bufferedData[data.getName().at(-1).toSegment()] = dataPtr;
+        },
+        [](const Data &, const security::v2::ValidationError &error) {
+            std::cout
+                << "xrdndnconsumer: Error while validating data for read: "
+                << error.getInfo() << std::endl;
+        });
+}
+
+ssize_t Consumer::Read(void *buff, off_t offset, size_t blen,
+                       std::string path) {
 
     uint64_t firstSegment = offset / MAX_NDN_PACKET_SIZE;
     uint64_t lastSegment = firstSegment + (blen / MAX_NDN_PACKET_SIZE);
@@ -140,24 +204,21 @@ ssize_t FileReader::Read(void *buff, off_t offset, size_t blen,
         Name name = Utils::getInterestUri(SystemCalls::read, path);
         name.appendSegment(i); // segment no.
 
-        Interest interest(name);
-        interest.setInterestLifetime(100_s);
-        interest.setMustBeFresh(true);
+        Interest readInterest = this->composeInterest(name);
 
-        std::cout << "xrdndnconsumer: Sending read file interest: " << interest
-                  << std::endl;
-
-        m_face.expressInterest(interest, bind(onData, _1, _2),
-                               bind(&FileReader::onNack, this, _1, _2),
-                               bind(&FileReader::onTimeout, this, _1));
+        std::cout << "xrdndnconsumer: Sending read file interest: "
+                  << readInterest << std::endl;
+        this->expressInterest(readInterest, SystemCalls::read);
     }
 
     m_face.processEvents();
 
+    this->saveDataInOrder(buff, offset, blen);
+
     return m_buffOffset;
 }
 
-void FileReader::saveDataInOrder(void *buff, off_t offset, size_t blen) {
+void Consumer::saveDataInOrder(void *buff, off_t offset, size_t blen) {
     auto storeInBuff = [&](const Block &content, off_t contentOffset) {
         size_t len = content.value_size() - contentOffset;
         len = len < blen ? len : blen;
@@ -180,5 +241,4 @@ void FileReader::saveDataInOrder(void *buff, off_t offset, size_t blen) {
         }
     }
 }
-
 } // namespace xrdndn

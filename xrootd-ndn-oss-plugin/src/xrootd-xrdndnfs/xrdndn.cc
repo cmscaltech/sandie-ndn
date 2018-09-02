0e317489114b10be85745c4d81d796e76c046f71
Catalin Iordache <catalinn.iordache@gmail.com>
2018-09-02 18:04:51 +0300

    XrdNdnOssPlugin: Comments style adjustments in xrdndn.cc

--- xrootd-ndn-oss-plugin/src/xrootd-xrdndnfs/xrdndn.cc
+++ xrootd-ndn-oss-plugin/src/xrootd-xrdndnfs/xrdndn.cc
@@ -1,22 +1,22 @@
-/**************************************************************************
- * Named Data Networking plugin for xrootd                                *
- * Copyright © 2018 California Institute of Technology                    *
- *                                                                        *
- * Author: Catalin Iordache <catalin.iordache@cern.ch>                    *
- *                                                                        *
- * This program is free software: you can redistribute it and/or modify   *
- * it under the terms of the GNU General Public License as published by   *
- * the Free Software Foundation, either version 3 of the License, or      *
- * (at your option) any later version.                                    *
- *                                                                        *
- * This program is distributed in the hope that it will be useful,        *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- * GNU General Public License for more details.                           *
- *                                                                        *
- * You should have received a copy of the GNU General Public License      *
- * along with this program.  If not, see <https://www.gnu.org/licenses/>. *
- **************************************************************************/
+/******************************************************************************
+ * Named Data Networking plugin for xrootd                                    *
+ * Copyright © 2018 California Institute of Technology                        *
+ *                                                                            *
+ * Author: Catalin Iordache <catalin.iordache@cern.ch>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ *****************************************************************************/
 
 #include <XrdSys/XrdSysError.hh>
 #include <XrdSys/XrdSysPlugin.hh>
@@ -40,10 +40,9 @@ XrdOss *XrdOssGetStorageSystem(XrdOss *native_oss, XrdSysLogger *Logger,
 }
 }
 
-// This function is called by the OFS layer to retrieve the Storage System
-// object. If a plugin library has been specified, then this function will
-// return the object provided by XrdOssGetStorageSystem() within the library.
-//
+/* This function is called by the OFS layer to retrieve the Storage System
+ * object. If a plugin library has been specified, then this function will
+ * return the object provided by XrdOssGetStorageSystem() within the library.*/
 static XrdOss *XrdOssGetSS(XrdOss *native_oss, XrdSysLogger *Logger,
                            const char *config_fn, const char *parms) {
     OssErouteMain.logger(Logger);

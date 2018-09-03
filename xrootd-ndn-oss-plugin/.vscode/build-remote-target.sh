055d47360ccf1836e327372e0ef4747a5709492f
Catalin Iordache <catalinn.iordache@gmail.com>
2018-09-03 13:57:22 +0300

    XrdNdn: VSCode project support cmake on launch tasks

--- xrootd-ndn-oss-plugin/.vscode/build-remote-target.sh
+++ xrootd-ndn-oss-plugin/.vscode/build-remote-target.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 remote_host=root@192.168.0.69
 projdirname=xrootd-ndn-oss-plugin
-
-echo $PWD
+buildir=$projdirname/build
 
 #copy new source files remote
 echo "copy new files on remote"
@@ -10,8 +9,12 @@ scp -r ../$projdirname $remote_host:.
 
 #compile new source files remote
 echo "compile new files on remote"
-ssh $remote_host "source /opt/rh/devtoolset-7/enable && zsh -l -c 'cd $projdirname && make -f Makefile $1'"
+ssh $remote_host "source /opt/rh/devtoolset-7/enable && \
+                zsh -l -c 'cd $buildir && \
+                            rm -rf CMake* && \
+                            cmake ../  && \
+                            make $1 VERBOSE=1'"
 
 #copy new binery from remote server
 echo "copy newly build $1 from remote server"
-scp $remote_host:./$projdirname/bin/$1 ./bin/
+scp $remote_host:./$buildir/$1 ./build/

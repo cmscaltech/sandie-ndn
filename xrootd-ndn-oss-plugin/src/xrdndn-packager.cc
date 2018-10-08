58ac388ecc8507283eefe85f95d762a6940f3c44
Catalin Iordache <catalinn.iordache@gmail.com>
2018-10-08 14:35:46 +0300

    XrdNdnProducer: Removed Dispatcher object and update Packager object

---
+++ xrootd-ndn-oss-plugin/src/xrdndn-packager.cc
@@ -0,0 +1,64 @@
+/******************************************************************************
+ * Named Data Networking plugin for xrootd                                    *
+ * Copyright © 2018 California Institute of Technology                        *
+ *                                                                            *
+ * Author: Catalin Iordache <catalin.iordache@cern.ch>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ *****************************************************************************/
+
+#include <math.h>
+
+#include "xrdndn-common.hh"
+#include "xrdndn-logger.hh"
+#include "xrdndn-packager.hh"
+
+using namespace ndn;
+
+namespace xrdndnproducer {
+Packager::Packager() {}
+Packager::~Packager() {}
+
+void Packager::digest(std::shared_ptr<ndn::Data> &data) {
+    data->setFreshnessPeriod(DEFAULT_FRESHNESS_PERIOD);
+    m_keyChain.sign(*data); // signWithDigestSha256
+}
+
+// Prepare Data containing an non/negative integer
+std::shared_ptr<Data> Packager::getPackage(const ndn::Name &name, int value) {
+    int type = value < 0 ? xrdndn::tlv::negativeInteger
+                         : xrdndn::tlv::nonNegativeInteger;
+    const Block content =
+        makeNonNegativeIntegerBlock(ndn::tlv::Content, fabs(value));
+
+    std::shared_ptr<Data> data = std::make_shared<Data>(name);
+    data->setContent(content);
+    data->setContentType(type);
+
+    boost::unique_lock<boost::shared_mutex> lock(m_mutex);
+    digest(data);
+    return data;
+}
+
+// Prepare Data as bytes
+std::shared_ptr<Data> Packager::getPackage(ndn::Name &name,
+                                           const uint8_t *value, ssize_t size) {
+    std::shared_ptr<Data> data = std::make_shared<Data>(name);
+    data->setContent(value, size);
+
+    boost::unique_lock<boost::shared_mutex> lock(m_mutex);
+    digest(data);
+    return data;
+}
+} // namespace xrdndnproducer
---
+++ xrootd-ndn-oss-plugin/src/xrdndn-packager.hh
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Named Data Networking plugin for xrootd                                    *
+ * Copyright © 2018 California Institute of Technology                        *
+ *                                                                            *
+ * Author: Catalin Iordache <catalin.iordache@cern.ch>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ *****************************************************************************/
+
+#ifndef XRDNDN_PACKAGER
+#define XRDNDN_PACKAGER
+
+#include <boost/thread/shared_mutex.hpp>
+#include <ndn-cxx/face.hpp>
+
+namespace xrdndnproducer {
+class Packager {
+  public:
+    Packager();
+    ~Packager();
+
+    std::shared_ptr<ndn::Data> getPackage(const ndn::Name &name, int value);
+    std::shared_ptr<ndn::Data> getPackage(ndn::Name &name, const uint8_t *value,
+                                          ssize_t size);
+
+  private:
+    void digest(std::shared_ptr<ndn::Data> &data);
+
+  private:
+    ndn::KeyChain m_keyChain;
+    mutable boost::shared_mutex m_mutex;
+};
+} // namespace xrdndnproducer
+
+#endif // XRDNDN_PACKAGER

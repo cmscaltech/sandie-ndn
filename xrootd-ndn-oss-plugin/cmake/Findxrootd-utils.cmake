d96610c94d0edf0945eefc1f00d3abbc57ea66c3
Catalin Iordache <catalinn.iordache@gmail.com>
2019-05-04 16:51:23 +0300

    Update cmake build system

--- xrootd-ndn-oss-plugin/cmake/Findxrootd-utils.cmake
+++ xrootd-ndn-oss-plugin/cmake/Findxrootd-utils.cmake
@@ -1,9 +1,74 @@
-find_path(XROOTD_INCLUDES XrdVersion.hh
-    HINTS "/usr/" "/opt/xrootd"
-    PATH_SUFFIXES "include/xrootd"
-    PATHS "/opt/xrootd")
+function(print_message mode VALUE)
+  if(NOT xrootd-utils_FIND_QUIETLY)
+    message(${mode} ${VALUE})
+  endif()
+endfunction()
 
+print_message(STATUS "Looking for XrdVersion.hh")
+find_path(XROOTD_INCLUDES XrdVersion.hh
+          HINTS "/usr/" "/opt/xrootd"
+          PATH_SUFFIXES "include/xrootd"
+          PATHS "/opt/xrootd")
+
+if(XROOTD_INCLUDES-NOTFOUND)
+  print_message(SEND_ERROR "Looking for XrdVersion.hh - not found")
+  set(XROOTD-NOTFOUND TRUE)
+else()
+  # Set XRootD version
+  file(READ "${XROOTD_INCLUDES}/XrdVersion.hh" xrootd-version-content)
+  string(REGEX MATCH
+               "XrdVERSION(.*)([0-9]+)\\.([0-9]+)\\.([0-9]+)[\"]"
+               XROOTD-VERSION
+               ${xrootd-version-content})
+
+  string(REPLACE "XrdVERSION "
+                 ""
+                 XROOTD-VERSION
+                 ${XROOTD-VERSION})
+  string(REPLACE "\""
+                 ""
+                 XROOTD-VERSION
+                 ${XROOTD-VERSION})
+  string(REPLACE "v"
+                 ""
+                 XROOTD-VERSION
+                 ${XROOTD-VERSION})
+
+  print_message(STATUS "Looking for XrdVersion.hh - found: ${XROOTD_INCLUDES}")
+  set(XROOTD-FOUND TRUE)
+endif(XROOTD_INCLUDES-NOTFOUND)
+
+print_message(STATUS "Looking for libXrdUtils")
 find_library(XROOTD_UTILS XrdUtils
-    HINTS "/usr/" "/opt/xrootd"
-    PATH_SUFFIXES "include/xrootd"
-    PATHS "/opt/xrootd")
+             HINTS "/usr/" "/opt/xrootd"
+             PATHS "/opt/xrootd")
+
+if(XROOTD_UTILS-NOTFOUND)
+  print_message(SEND_ERROR "Looking for libnXrdUtils- not found")
+  set(XROOTD-NOTFOUND TRUE)
+else()
+  print_message(STATUS "Looking for libXrdUtils - found: ${XROOTD_UTILS}")
+  set(XROOTD-FOUND TRUE)
+endif(XROOTD_UTILS-NOTFOUND)
+
+if(XROOTD_UTILS-NOTFOUND)
+  if(xrootd-utils_FIND_REQUIRED)
+    message(SEND_ERROR "XRootD package not found")
+  else()
+    print_message(SEND_ERROR "XRootD package not found")
+  endif()
+else()
+  print_message(STATUS "XRootD version: ${XROOTD-VERSION}")
+endif()
+
+if(xrootd-utils_FIND_VERSION_EXACT)
+  if(NOT XROOTD-VERSION VERSION_EQUAL xrootd-utils_FIND_VERSION)
+    message(FATAL_ERROR "XRootD package version: " ${xrootd-utils_FIND_VERSION}
+                        " not found")
+  endif()
+else()
+  if(XROOTD-VERSION VERSION_LESS xrootd-utils_FIND_VERSION)
+    message(FATAL_ERROR "Require at least XRootD version "
+                        ${xrootd-utils_FIND_VERSION})
+  endif()
+endif()

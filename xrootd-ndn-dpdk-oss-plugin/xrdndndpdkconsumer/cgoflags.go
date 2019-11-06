087e77720b6120eb5e4411787239f50cb087cc9d
Catalin Iordache <caiordache@bitdefender.com>
2019-11-06 17:15:49 +0200

    xrdndndpdk: Fix type in cgoflags

--- xrootd-ndn-dpdk-oss-plugin/xrdndndpdkconsumer/cgoflags.go
+++ xrootd-ndn-dpdk-oss-plugin/xrdndndpdkconsumer/cgoflags.go
@@ -2,6 +2,6 @@ package xrdndndpdkconsumer
 
 /*
 #cgo CFLAGS:-Werror -Wno-error=deprecated-declarations -m64 -pthread -O3 -g -march=native -I/usr/local/include/dpdk -I/usr/include/dpdk
-#cgo LDFLAGS:-L${SRCDIR}/../../../ndn-dpdk/build/ -L${SRCDIR}/../libs/ -lxrdndndpdk-common -lndn-dpdk-iface -lndn-dpdk-ndn -lndn-dpdk-dpdk -lndn-dpdk-core -L/usr/local/libv -lurcu-qsbr -lurcu-cds -lubpf -lspdk -lspdk_env_dpdk -ldpdk -lnuma -lm
+#cgo LDFLAGS:-L${SRCDIR}/../../../ndn-dpdk/build/ -L${SRCDIR}/../libs/ -lxrdndndpdk-common -lndn-dpdk-iface -lndn-dpdk-ndn -lndn-dpdk-dpdk -lndn-dpdk-core -L/usr/local/lib -lurcu-qsbr -lurcu-cds -lubpf -lspdk -lspdk_env_dpdk -ldpdk -lnuma -lm
 */
 import "C"
